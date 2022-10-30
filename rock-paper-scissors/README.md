0fc71ea10f06f6863bb3ebbfb54bc769db67176e
Justin V <nnfr18@gmail.com>
2022-10-29 23:45:52 -0400

    Added random AI choice and moved README file location

---
+++ rock-paper-scissors/README.md
@@ -0,0 +1,3 @@
+# rock-paper-scissors
+
+Simple game of rock paper scissors vs an AI. First real program using javascript. Game is played and displayed using only the console on a browser.
--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+
+getComputerChoice();
+
+//AI's random choice 
+function getComputerChoice() {
+    var result = ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
+    console.log(result);
+}
