9a692875b3e9f86457817cd7db54c122076d233a
Justin V <nnfr18@gmail.com>
2022-10-30 17:45:07 -0400

    commit before push program is fully functional with correct output

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -7,14 +7,12 @@ game();
 //Player Choice
 function getPlayerChoice() {
 	var result = prompt("Choose rock, paper, or scissors");
-	console.log("Player: " + result);
 	return result;
 }
 
 //AI's random choice 
 function getComputerChoice() {
 	var result = ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
-	console.log("Computer: " + result);
 	return result;
 }
 
@@ -24,30 +22,37 @@ function playRound() {
 	let computerSelection = getComputerChoice();
 
    if (playerSelection === "rock" && computerSelection === "scissors") {
+	console.log("Player chose " + playerSelection + " Computer chose " + computerSelection);
 	console.log("Player wins this round!");
 	return playerScore++;
 
    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+		console.log("Player chose " + playerSelection + " Computer chose " + computerSelection);
 		console.log("Computer wins this round!");
    		return computerScore++;
 
    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+		console.log("Player chose " + playerSelection + " Computer chose " + computerSelection);
 		console.log("Player wins this round!");
 		return playerScore++;
 
    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+		console.log("Player chose " + playerSelection + " Computer chose " + computerSelection);
 		console.log("Computer wins this round!");
 		return computerScore++;
 
 	} else if (playerSelection === "paper" && computerSelection === "rock") {
+		console.log("Player chose " + playerSelection + " Computer chose " + computerSelection);
 		console.log("Player wins this round!");
 		return playerScore++;
 		
 	} else if (playerSelection === "rock" && computerSelection === "paper") {
+		console.log("Player chose " + playerSelection + " Computer chose " + computerSelection);
 		console.log("Computer wins this round!");
 		return computerScore++;
 	
 	}  else {
+		console.log("Player chose " + playerSelection + " Computer chose " + computerSelection);
 		console.log("You tie this round!");
 	}
 }
@@ -55,9 +60,7 @@ function playRound() {
 //play 5 rounds of game, total score and declare winner at the end
 function game() {
 	for (let i = 0; i < 5; i++) {
-		getPlayerChoice();
-		getComputerChoice();
 		playRound();	
 	}
-	console.log("Game Over total score: Player:" + playerScore + " Computer:" + computerScore);
+	console.log("Game Over best of five final score: Player:" + playerScore + " Computer:" + computerScore);
 }
