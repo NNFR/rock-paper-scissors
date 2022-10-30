7d689c65d99ba7cdc68d90c891325108065de55d
Justin V <nnfr18@gmail.com>
2022-10-30 00:32:01 -0400

    added  play single game function to test correct output

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,8 +1,27 @@
 
-getComputerChoice();
-
 //AI's random choice 
 function getComputerChoice() {
-    var result = ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
-    console.log(result);
+	var result = ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
+	return result;
 }
+
+//single round of game
+function playRound(playerSelection, computerSelection) {
+   if (playerSelection == "rock" && computerSelection == "scissors") {
+	return "You win! Rock beats Scissors";
+   } else if (playerSelection == "scissors" && computerSelection == "rock") {
+   		return "You lose! Rock beats Scissors";
+   } else if (playerSelection == "scissors" && computerSelection == "paper") {
+		return "You Win! Scissors beats Paper";
+   } else if (playerSelection == "paper" && computerSelection == "scissors") {
+		return "You lose! Scissors beats Paper";
+	} else if (playerSelection == "paper" && computerSelection == "rock") {
+		return "You Win! Paper beats Rock";
+	} else if (playerSelection == "rock" && computerSelection == "paper") {
+		return "You lose! Paper beats Rock";
+	}
+}
+
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection.toLowerCase(), computerSelection));
